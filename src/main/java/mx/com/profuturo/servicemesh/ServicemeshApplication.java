8d8c54a32f4487ffe8cc92e01662a01e15a839ad
Spencer <isc.spencer@gmail.com>
2020-10-29 16:37:39 -0600

    dependency8

--- src/main/java/mx/com/profuturo/servicemesh/ServicemeshApplication.java
+++ src/main/java/mx/com/profuturo/servicemesh/ServicemeshApplication.java
@@ -3,8 +3,10 @@ package mx.com.profuturo.servicemesh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @SpringCloudApplication
+@Configuration
 @EnableFeignClients
 public class ServicemeshApplication {
 	public static void main(String[] args) {
