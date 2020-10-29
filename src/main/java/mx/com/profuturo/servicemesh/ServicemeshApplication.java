8f7023fe7ee0f2c24cc3dcba626a4ad84877743d
Spencer <isc.spencer@gmail.com>
2020-10-29 17:53:22 -0600

    version

--- src/main/java/mx/com/profuturo/servicemesh/ServicemeshApplication.java
+++ src/main/java/mx/com/profuturo/servicemesh/ServicemeshApplication.java
@@ -1,15 +1,11 @@
 package mx.com.profuturo.servicemesh;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
 
-@SpringCloudApplication
+@SpringBootApplication
 @EnableFeignClients
-@Configuration
-@EnableAutoConfiguration
 public class ServicemeshApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServicemeshApplication.class, args);
